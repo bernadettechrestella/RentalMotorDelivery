1ffd29e295aab6656443525c244485dc900525c0
Stella <Stella @DESKTOP-NREV4QF>
2018-11-06 10:31:42 +0700

    versi 3

--- RentalMotor/src/java/services/motorResource.java
+++ RentalMotor/src/java/services/motorResource.java
@@ -13,7 +13,6 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
-import static javax.ws.rs.HttpMethod.POST;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
